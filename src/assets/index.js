2496568e487955271567c4fba2dabd851b602a79
noel <noeldenista97@gmail.com>
2022-04-04 16:02:54 +0700

    Make page login

---
+++ src/assets/index.js
@@ -0,0 +1,5 @@
+/* eslint-disable prettier/prettier */
+const Images = {
+  ILLogin: require('./illustration/loginImage.png'),
+};
+export default Images;
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,12 +1,45 @@
 /* eslint-disable prettier/prettier */
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TextInput,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
+import React,{useState} from 'react';
+import Images from '../../assets';
 
-const Login = () => {
+const Login = ({navigation}) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const login = () => {
+    navigation.replace('Home');
+  };
+
   return (
-    <View style={styles.pages}>
-      <Text>Hello Word</Text>
-    </View>
+    <SafeAreaView style={styles.pages}>
+      <View style={styles.container}>
+        <Image source={Images.ILLogin} style={styles.image}/>
+        <Text style={styles.title}>Camp404 Store</Text>
+        <TextInput
+        placeholder="Email"
+        keyboardAppearance="email-address"
+        onChangeText={setEmail}
+        value={email}
+        style={styles.emailInput}/>
+        <TextInput
+        placeholder="Password"
+        onChangeText={setPassword}
+        value={password}
+        style={styles.passwordInput}/>
+        <View style={styles.breakLine}/>
+        <TouchableOpacity style={styles.button} onPress={login}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 };
 
@@ -15,7 +48,61 @@ export default Login;
 const styles = StyleSheet.create({
   pages: {
     flex:1,
+    //justifyContent:'center',
+    //alignItems:'center',
+    backgroundColor:'#1F8597',
+  },
+  container : {
+    flex:1,
+    paddingHorizontal:16,
+    justifyContent:'center',
+  },
+  image: {
+    alignSelf:'center',
+  },
+  title:{
+    fontSize:24,
+    fontWeight:'bold',
+    color:'#F2F2F2',
+    alignSelf:'center',
+    marginBottom:77,
+    marginTop:16,
+  },
+  emailInput:{
+    backgroundColor:'#FFFFFF',
+    height:45,
+    borderRadius:8,
+    paddingHorizontal:16,
+    paddingVertical:13,
+    borderWidth:1,
+    marginBottom:16,
+    borderColor:'#F4A896',
+  },
+  passwordInput:{
+    backgroundColor:'#FFFFFF',
+    height:45,
+    borderRadius:8,
+    paddingHorizontal:16,
+    paddingVertical:13,
+    borderWidth:1,
+    borderColor:'#F4A896',
+  },
+  breakLine:{
+    backgroundColor:'#C4C4C4',
+    marginVertical:40,
+    marginHorizontal:16,
+    height:1,
+  },
+  button:{
+    backgroundColor:'#F4A896',
+    height:45,
+    borderRadius:8,
     justifyContent:'center',
     alignItems:'center',
+  },
+  buttonText: {
+    fontSize:16,
+    color:'#fff',
+    fontWeight:'bold',
   },
 });
