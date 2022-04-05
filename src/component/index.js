10fc947c7f244eeca5273125ba5df249e5bf5562
noel <noeldenista97@gmail.com>
2022-04-05 16:26:44 +0700

    add flat list with dummy data

--- src/component/index.js
+++ src/component/index.js
@@ -1,4 +1,4 @@
 /* eslint-disable prettier/prettier */
 import Header from './Header';
-
-export {Header};
+import ProductCard from './ProductCard';
+export {Header, ProductCard};
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,54 @@
 /* eslint-disable prettier/prettier */
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
-import { Header } from '../../component';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { Header, ProductCard} from '../../component';
+//import { SafeAreaView } from 'react-native-safe-area-context';
 
+const dummy = [
+  {
+    id:1,
+    title:'Product Name',
+    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting',
+    price:'50000',
+    image:'https://source.unsplash.com//1600x900/?shoes',
+  },
+  {
+    id:2,
+    title:'Product Name',
+    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting',
+    price:'50000',
+    image:'https://source.unsplash.com//1600x900/?shoes',
+  },
+  {
+    id:3,
+    title:'Product Name',
+    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting',
+    price:'50000',
+    image:'https://source.unsplash.com//1600x900/?shoes',
+  },
+];
+
+const renderItem = ({item}) => (
+  <ProductCard
+    title={item.title}
+    desc={item.desc}
+    price={item.price}
+    image={item.image}
+  />
+);
 const Home = () => {
   return (
     <SafeAreaView style={styles.page}>
         <Header title={'Home'}/>
+    <FlatList
+        showsVerticalScrollIndicator={false}
+        data={dummy}
+        renderItem={renderItem}
+        keyExtractor={item => item.id.toString()}
+        ListHeaderComponent={<Text style={styles.label}> List Product </Text>}
+        ListFooterComponent={<View style={styles.footer}/>}
+        style={styles.container}
+    />
     </SafeAreaView>
   );
 };
@@ -18,5 +59,18 @@ const styles = StyleSheet.create({
     page:{
         flex:1,
         backgroundColor:'#fff',
+    },
+    container:{
+      backgroundColor:'#fff',
+      padding:16,
+    },
+    label:{
+      fontSize:16,
+      fontWeight:'bold',
+      color:'#2F2E41',
+      marginBottom:16,
+    },
+    footer:{
+      height:30,
     },
 });
