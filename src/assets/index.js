306eeaab304f48ac90639dd85a4b1d15a534d85d
noel <noeldenista97@gmail.com>
2022-04-07 15:05:11 +0700

    add lib bottom nav + add icon home & setting

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,7 @@
 /* eslint-disable prettier/prettier */
 const Images = {
   ILLogin: require('./illustration/loginImage.png'),
+  ICHome: require('./bottom-nav/home.png'),
+  ICSetting: require ('./bottom-nav/setting.png'),
 };
 export default Images;
